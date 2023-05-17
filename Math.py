73f3cd7335af375bcc3ceaec326bbccd19f7a34e
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-05-17 14:46:25 +0000

    h

--- Math.py
+++ Math.py
@@ -3,6 +3,15 @@ import math
 x = 1
 h = int(input(": "))
 
-for i in range(h):
-    pi = (4/x)
-    x + 2
+#for i in range(h):
+    #pi = (4/x)
+
+#print(pi)
+
+
+
+def sigma(first, last, const):
+    sum = 0
+    for i in range(first, last + 1):
+        sum += const * i
+    return sum
